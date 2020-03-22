0d7d7c50c433e5783885b26ea7cb298d8ec6622c
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-22 18:58:17 -0400

    Finished up
    -Added client to server interaction
    -Added server to client interaction

--- ClientServer/src/Client.java
+++ ClientServer/src/Client.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -7,13 +9,17 @@ public class Client
 {
 	public static void main(String[] args) throws UnknownHostException, IOException
 	{
+		//Socket that client connects to
 		Socket socket = new Socket("localhost",4999);
 		
 		PrintWriter pr = new PrintWriter(socket.getOutputStream());
-		pr.println("Test message");
-		
-		
-		
+		pr.println("This a test message from the client");
 		pr.flush();
+		
+		InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+		
+		BufferedReader reader = new BufferedReader(isr);
+		String message = reader.readLine();
+		System.out.println("Server said: " + message);
 	}
 }
--- ClientServer/src/Server.java
+++ ClientServer/src/Server.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -9,15 +10,27 @@ public class Server
 {
 	public static void main(String[] args) throws UnknownHostException, IOException
 	{
+		//socket that the server connects to, same as the socket
+		//the client connects to
 		ServerSocket serverSocket = new ServerSocket(4999);
+		
+		//When Client.java is run, it will connect to the server 
+		//socket above, this below line waits and listens for this event
+		//to occur
 		Socket socket = serverSocket.accept();
-
+		
+		//Will only print when the client connects
 		System.out.println("Client Connected");
 		
+		//Input stream comes from the accepted socket
 		InputStreamReader isr = new InputStreamReader(socket.getInputStream());
 		
 		BufferedReader reader = new BufferedReader(isr);
 		String message = reader.readLine();
-		System.out.println("client : " + message);
+		System.out.println("client said: " + message);
+		
+		PrintWriter pr = new PrintWriter(socket.getOutputStream());
+		pr.println("This is a test message from the server");
+		pr.flush();
 	}
 }
